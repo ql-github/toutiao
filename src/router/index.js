4991d4e8390b548391a2f5613466c0f4c71b6e75
ql-github <905972934@qq.com>
2019-07-09 17:43:21 +0800

    登陆模块表单绘制完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 // 路由配置
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 Vue.use(VueRouter)
+import Login from '@/view/login'
 
 const router = new VueRouter({
-  routes: []
+  routes: [{name:'login',path:'/login',component:Login}]
 })
 
 export default router
---
+++ src/view/login/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="login-container">
+    <el-card class="box-card">
+      <img src="../../assets/images/logo_index.png" alt />
+      <!-- 表单 -->
+      <el-form>
+        <el-form-item>
+          <el-input placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input placeholder="请输入验证码" style="width:280px"></el-input>
+          <el-button style="float:right">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="checked"></el-checkbox>
+          <span>我已阅读并同意</span>&nbsp;&nbsp;
+          <el-link type="primary">用户协议</el-link>&nbsp;&nbsp;
+          和&nbsp;&nbsp;<el-link type="primary">隐私条款</el-link>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      checked: true
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background: url(../../assets/images/login_bg.jpg), no-repeat center/cover;
+  .box-card {
+    width: 450px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 200px;
+      margin: 10px auto 30px;
+    }
+    .el-form{
+      .el-form-item{
+        .el-link{
+          vertical-align: baseline;
+        }
+      }
+    }
+  }
+}
+</style>
